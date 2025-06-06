897fc06078d13f15d2f1a2ef3a583860c2c9e74c
rezazovich1 <96366035+rezazovich1@users.noreply.github.com>
2025-06-06 23:51:30 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, UploadFile, File
 import os, shutil
+from typing import List  # 👈 اضافه شده
 from analyzer import find_best_resume
 
 app = FastAPI()
@@ -8,7 +9,7 @@ UPLOAD_FOLDER = "resumes"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.post("/upload/")
-async def upload_resumes(files: list[UploadFile] = File(...)):
+async def upload_resumes(files: List[UploadFile] = File(...)):  # 👈 اینجا تغییر داده شده
     saved_files = []
     for file in files:
         file_path = os.path.join(UPLOAD_FOLDER, file.filename)
